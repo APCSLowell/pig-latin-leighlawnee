0ee0806c330f62ba6fcf2ac11d9eaf46d83172ce
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-01-25 12:26:56 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -28,10 +28,12 @@ public class PigLatin {
 	    }
     }
     public int findFirstVowel(String sWord) {
+	    for (int i = 0; i < sWord.length(); i++) {
       if (sWord.substring(i, i+1).equals("a") || sWord.substring(i, i+1).equals("e") || sWord.substring(i, i+1).equals("i")|| sWord.substring(i, i+1).equals("o")|| sWord.substring(i, i+1).equals("u")) {
       return i;
     }
       return -1;
+    }
     }
     public String pigLatin(String sWord) {
 	    if(findFirstVowel(sWord) == -1) 
