628cfa3c65a233d4ea45f1026e842ca9a1df6a8b
leighlawnee <143223104+leighlawnee@users.noreply.github.com>
2024-01-25 12:24:29 -0800

    Update PigLatin.java

--- src/main/java/PigLatin.java
+++ src/main/java/PigLatin.java
@@ -31,18 +31,16 @@ public class PigLatin {
       if (sWord.substring(i, i+1).equals("a") || sWord.substring(i, i+1).equals("e") || sWord.substring(i, i+1).equals("i")|| sWord.substring(i, i+1).equals("o")|| sWord.substring(i, i+1).equals("u")) {
       return i;
     }
-  }
       return -1;
     }
-
     public String pigLatin(String sWord) {
 	    if(findFirstVowel(sWord) == -1) 
-      return sWord + "ay";
-      else if (findFirstVowel(sWord) == 0)
-      return sWord + "way";
+      		return sWord + "ay";
+      	else if (findFirstVowel(sWord) == 0)
+      		return sWord + "way";
       else if (sWord.substring(0, 2).equals("qu"))
-      return sWord.substring(2) + "quay";
+      	return sWord.substring(2) + "quay";
       else 
-      return(sWord.substring(findFirstVowel(sWord), sWord.length()) + sWord.substring(0, findFirstVowel(sWord)) + "ay");
+      	return(sWord.substring(findFirstVowel(sWord), sWord.length()) + sWord.substring(0, findFirstVowel(sWord)) + "ay");
     }
 }//end PigLatin class
